4de2302727e8b6934c83975f195d8c0d3c70d97c
bsun94 <52805678+bsun94@users.noreply.github.com>
2020-08-05 22:28:31 -0400

    COVIDTracker and gitignore updated

--- COVIDTracker.py
+++ COVIDTracker.py
@@ -16,17 +16,16 @@ from datetime import datetime
 from selenium import webdriver as wd
 import sys
 
-os.chdir(os.getcwd())
-
 class COVID(object):
     
     # Don't let users pass urls into object - store them as defaults; remaining code cleaning up data is specific to formats of the sources
     # Further, good, freely-available COVID data sources are less prevalent than one'd think
+    COVID_URL     = 'https://covid.ourworldindata.org/data/owid-covid-data.csv'
+    GEOJSON_URL   = 'https://raw.githubusercontent.com/datasets/geo-countries/master/data/countries.geojson'
+    CENTROIDS_URL = 'http://developers.google.com/public-data/docs/canonical/countries_csv/'
+    os.chdir(os.getcwd())
+    
     def __init__(self, year=None, month=None, day=None, map_type='Cases'):
-        self.COVID_URL     = 'https://covid.ourworldindata.org/data/owid-covid-data.csv'
-        self.GEOJSON_URL   = 'https://raw.githubusercontent.com/datasets/geo-countries/master/data/countries.geojson'
-        self.CENTROIDS_URL = 'http://developers.google.com/public-data/docs/canonical/countries_csv/'
-        
         self.covid_df            = pd.DataFrame([])
         self.geo_data            = pd.DataFrame([])
         self.name_iso2_mapping   = {}
@@ -34,7 +33,6 @@ class COVID(object):
         
         try:
             self.date = datetime(year=year, month=month, day=day)
-        
         except:
             print('Invalid date entry (year, month, day take valid int inputs)! Date defaulted to today.')
             self.date = datetime.today()
@@ -45,7 +43,6 @@ class COVID(object):
         
         if map_type not in ['Cases', 'Deaths']:
             sys.exit('Please specify either "Cases" or "Deaths" as map type!')
-        
         else:
             self.map_type = map_type
     
@@ -54,29 +51,26 @@ class COVID(object):
     def webScraper(self):
         try:
             self.covid_df = pd.read_csv(self.COVID_URL)
-            latest_date   = self.covid_df['date'].max()
-            earliest_date = self.covid_df['date'].min()
-            self.covid_df = self.covid_df[self.covid_df['date']==self.date.strftime('%Y-%m-%d')]
-            
-            if self.covid_df.empty:
-                exit_string = 'Requested date not available. Latest date available is ' + latest_date + ' while earliest is ' + earliest_date
-                sys.exit(exit_string)
-            
-            else:
-                self.covid_df = self.covid_df[self.covid_df['location']!='World']
-        
         except:
             sys.exit('COVID data is unavailable at source.')
+            
+        latest_date   = self.covid_df['date'].max()
+        earliest_date = self.covid_df['date'].min()
+        self.covid_df = self.covid_df[self.covid_df['date'] == self.date.strftime('%Y-%m-%d')]
+        
+        if self.covid_df.empty:
+            exit_string = 'Requested date not available. Latest date available is ' + latest_date + ' while earliest is ' + earliest_date
+            sys.exit(exit_string)
+        else:
+            self.covid_df = self.covid_df[self.covid_df['location']!='World']
         
         try:
             self.countries_centroids = pd.read_html(self.CENTROIDS_URL, header=0, index_col='country')[0]
-        
         except:
             sys.exit('Central coordinates data for countries unavailable from Google developers.')
         
         try:
             self.geo_data = requests.get(self.GEOJSON_URL).json()
-        
         except:
             sys.exit('GeoJSON data unavailable to draw country polygons.')
     
@@ -85,14 +79,13 @@ class COVID(object):
     def writeCountryCodeFile(self):
         try:
             geojson = requests.get(self.GEOJSON_URL).json()
-        
         except:
             sys.exit('GeoJSON data unavailable at source.')
         
         country_mapping = {}
         for country in geojson['features']:
+            iso_2        = country['properties']['ISO_A2']
             country_name = country['properties']['ADMIN']
-            iso_2 = country['properties']['ISO_A2']
             country_mapping.update({country_name: iso_2})
         
         with open('countryNameISO2.json', 'w') as file:
@@ -118,25 +111,29 @@ class COVID(object):
             print('Re-importing required JSONs...')
             self.updateCountryNames()
     
+    # Makes naming map file easier for code later on
+    def generateFileName(self):
+        return 'Covid' + self.map_type + '.html'
+    
     # Plot choropleth map for total COVID cases/deaths, marking top 10 countries
     def drawMap(self):
-        world_map = folium.Map(location=[25, 10], zoom_start=3)
+        world_map     = folium.Map(location=[25, 10], zoom_start=3)
         totals_column = 'total_' + self.map_type.lower()
         
         if self.map_type == 'Cases':
+            SCALE        = 10 ** 6
+            UNITS        = 'M'
             color_scheme = 'YlOrRd'
-            SCALE = 10 ** 6
-            UNITS = 'M'
         
         else:
+            SCALE        = 10 ** 3
+            UNITS        = 'K'
             color_scheme = 'PuRd'
-            SCALE = 10 ** 3
-            UNITS = 'K'
         
-        top10 = self.covid_df.sort_values(totals_column, axis=0, ascending=False)['location'][:10]
-        bins = list(np.linspace(0, np.ceil(self.covid_df[totals_column].max() / SCALE) * SCALE, 6))
-        legend_name = 'Total Number of COVID-19 ' + self.map_type
-        map_file_name = 'Covid' + self.map_type + '.html'
+        top10         = self.covid_df.sort_values(totals_column, axis=0, ascending=False)['location'][:10]
+        bins          = list(np.linspace(0, np.ceil(self.covid_df[totals_column].max() / SCALE) * SCALE, 6))
+        legend_name   = 'Total Number of COVID-19 ' + self.map_type
+        map_file_name = self.generateFileName()
         
         folium.Choropleth(geo_data=self.geo_data,
                           data=self.covid_df, 
@@ -148,14 +145,14 @@ class COVID(object):
                           highlight=True
                           ).add_to(world_map)
         
-        for i in range(len(top10)):
+        for i in range(10):
             country = top10.iloc[i]
-            cases = self.covid_df[self.covid_df['location']==country][totals_column] / SCALE
+            cases   = self.covid_df[self.covid_df['location'] == country][totals_column] / SCALE
             
             # Centroid coordinates for each country labelled by its ISO-2 code
-            lat = self.countries_centroids.loc[self.name_iso2_mapping[country]]['latitude']
-            long = self.countries_centroids.loc[self.name_iso2_mapping[country]]['longitude']
-            popup = '{0:s}: {1:.2f}{2:s} total {3:s}'.format(country, cases.values[0], UNITS, self.map_type.lower())
+            lat   = self.countries_centroids.loc[self.name_iso2_mapping[country]]['latitude']
+            long  = self.countries_centroids.loc[self.name_iso2_mapping[country]]['longitude']
+            popup = f"{country}: {cases.values[0]:.2f}{UNITS} total {self.map_type.lower()}"
             
             folium.Marker(location=[lat, long],
                           popup=folium.Popup(popup, 
@@ -165,7 +162,7 @@ class COVID(object):
             world_map.save(map_file_name)
     
     def displayMap(self):
-        filepath = os.getcwd() + '/Covid' + self.map_type + '.html'
+        filepath = os.getcwd() + '/' + self.generateFileName()
         
         if not os.path.exists(filepath):
             sys.exit('Desired map has not yet been created! Did you change map type midway?')
